ffb4aa5cad4c8f3f7894940a72315d41da5a392e
Rohit Parakh <rohitparakh4@gmail.com>
2022-02-04 12:18:17 +0530

    change require to import for path

--- server/index.js
+++ server/index.js
@@ -1,3 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Socket file
+
+import { Server } from 'socket.io';
+
+const io = new Server(9000, {
+    cors: {
+        origin: 'https://whatsapp-clone-rohit.netlify.app/',
+    }, 
+})
+
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId === userId) && users.push({ userId, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+}
+
+io.on('connection',  (socket) => {
+    console.log('user connected')
+
+    //connect
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send message
+    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        console.log(users)
+        io.to(user.socketId).emit('getMessage', {
+            senderId, text
+        })
+    })
+
+    //disconnect
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    })
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Server file
+
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -10,7 +66,7 @@ import Routes from './routes/Routes.js';
 dotenv.config();
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // const username = process.env.DB_USERNAME;
 // const password = process.env.DB_PASSWORD;
@@ -22,4 +78,12 @@ app.listen(PORT, () => console.log(`Server is running successfully on PORT ${POR
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+if ( process.env.NODE_ENV =="production" ){
+    app.use(express.static("client/build"));
+    import * as path from 'path';
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client/build/index.html"));
+    })
+}
 app.use('/', Routes);
